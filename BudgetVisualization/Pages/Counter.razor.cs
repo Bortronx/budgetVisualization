a473d7eea0437ba2aa8556cb6996a85351f1aa23
bortronx@gmail.com <bortronx@gmail.com>
2020-08-29 13:05:23 -0400

    Initial Template

---
+++ BudgetVisualization/Pages/Counter.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetVisualization.Pages
+{
+    public partial class Counter
+    {
+
+        private string item1Name = "PoliceCar";
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        protected override void OnInitialized()
+        {
+            System.Console.WriteLine("Hello");
+
+
+        }
+    }
+}
