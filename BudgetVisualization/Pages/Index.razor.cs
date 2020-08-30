e80be5cf96be2bce96edd5cbb9d3cf46a643ae4c
xtine <christine@usmagency.com>
2020-08-30 18:10:50 -0400

    added method to calculate comparison result

--- BudgetVisualization/Pages/Index.razor.cs
+++ BudgetVisualization/Pages/Index.razor.cs
@@ -15,6 +15,10 @@ namespace BudgetVisualization.Pages
 
         public int Width { get; set; }
 
+        public float CalculatedComparisonValue { get; set; }
+
+        public string ComparisonUnit { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             // Subscribe to the StateChanged EventHandler
@@ -29,5 +33,25 @@ namespace BudgetVisualization.Pages
         // invokes its StateHasChanged() method
         void AppStateHasChanged(
             object sender, EventArgs e) => StateHasChanged();
+
+
+        //caluclating comparison (we will probably have to change this based on what the data we are getting looks like)
+
+        //what we want:
+        //calculate the amount of a community item that can be funded using the funds from a police item
+
+        //what we need:
+        //selection type: unit (i.e. years, buildings, etc.)
+        //cost of a single unit of community item (1 year of employment training = $10,000 [CommunityBudgetItemValue])
+        //total cost of a set number of police items (i.e. 4 helicopters = $315,000 [PoliceBudgetItemValue])
+
+        public void GetComparisonResult(float CommunityBudgetItemValue, float PoliceBudgetItemValue,
+               string SelectionType)
+        {
+            this.CalculatedComparisonValue = PoliceBudgetItemValue / CommunityBudgetItemValue;
+            this.ComparisonUnit = SelectionType;
+        }
+
+
     }
 }
