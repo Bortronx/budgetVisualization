e3099916d231f1a3b06d2e43e9ccca7a12781af9
bortronx@gmail.com <bortronx@gmail.com>
2020-09-04 20:26:44 -0400

    Added localization to landing page and localization menu to website

--- BudgetVisualization/Program.cs
+++ BudgetVisualization/Program.cs
@@ -1,13 +1,12 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 using BudgetVisualization.Services;
 using BudgetVisualization.Data;
+using System.Globalization;
 
 namespace BudgetVisualization
 {
@@ -19,12 +18,24 @@ namespace BudgetVisualization
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             builder.Services.AddScoped<BrowserService>();
-
             builder.Services.AddScoped<BudgetData>();
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+
+            var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+
+            if (result != null)
+            {
+                var culture = new CultureInfo(result);
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+            }
+
+            await host.RunAsync();
         }
     }
 }
