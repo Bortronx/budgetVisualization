c2f3e45a1f3ccdce0e0309f09245d242345ae839
bortronx <bortronx@gmail.com>
2020-09-15 21:30:45 -0400

    Fixed Text to include latest links. Added localization syntax but localization is not working .resx file not being found

--- BudgetVisualization/Resources/Index.Designer.cs
+++ BudgetVisualization/Resources/Index.Designer.cs
@@ -22,14 +22,14 @@ namespace BudgetVisualization.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class App {
+    public class Index {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal App() {
+        internal Index() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace BudgetVisualization.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BudgetVisualization.Resources.App", typeof(App).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BudgetVisualization.Resources.Index", typeof(Index).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
