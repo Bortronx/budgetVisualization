1be953e4d29a993216449293cd3d7af6def97bf0
bortronx <bortronx@gmail.com>
2021-03-09 16:35:32 -0500

    Added more localization. Reworked component to handle localizaiton

---
+++ BudgetVisualization/Components/ProposedBudgetItem.razor.cs
@@ -0,0 +1,26 @@
+﻿using BudgetVisualization.Data;
+using BudgetVisualization.Pages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetVisualization.Components
+{
+    public partial class ProposedBudgetItem
+    {
+        [Parameter]
+        public ProposedItem itemData { get; set; }
+
+        [Parameter]
+        public IStringLocalizer Localizer { get; set; }
+
+        string color;
+
+        string defaultColor = "#accce7";
+        string activeColor = "#86abca";
+
+    }
+}
