336868905457cfc177d0fce1e8fc637ddc179efb
bortronx <bortronx@gmail.com>
2021-04-21 14:14:40 -0400

    Added Take Action section

---
+++ BudgetVisualization/Components/CampaignSection.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace BudgetVisualization.Components
+{
+    public partial class CampaignSection
+    {
+        [Parameter]
+        public string CampaignName { get; set; }
+
+        [Parameter]
+        public RenderFragment CampaignDescription { get; set; }
+
+        [Parameter]
+        public string CampaignActionTitle { get; set; }
+
+        [Parameter]
+        public RenderFragment CampaignActionContent { get; set; }
+      
+        [Parameter]
+        public string CampaignImage { get; set; } // TODO: Implement
+    }
+}
